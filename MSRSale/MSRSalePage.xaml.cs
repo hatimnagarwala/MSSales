f816422da673c26eaa7d207e5f19b3d14d3064df
hatim nagarwala <a-hanaga@microsoft.com>
2017-05-29 11:57:08 +0400

    new commit

--- MSRSale/MSRSalePage.xaml.cs
+++ MSRSale/MSRSalePage.xaml.cs
@@ -1,4 +1,6 @@
-﻿using Xamarin.Forms;
+﻿using System;
+using System.Collections.Generic;
+using Xamarin.Forms;
 
 namespace MSRSale
 {
@@ -7,6 +9,19 @@ namespace MSRSale
         public MSRSalePage()
         {
             InitializeComponent();
+            loadPickerData();
+
+        }
+
+        private void loadPickerData()
+        {
+            List<string> locations = new List<string>();
+            locations.Add("Carrefour");
+            locations.Add("Emax");
+            locations.Add("Jumbo");
+
+
+            LocationPicker.ItemsSource = locations;
         }
     }
 }

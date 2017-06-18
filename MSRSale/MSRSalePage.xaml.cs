cdc847534467f9ec90197f80739ce82454b446ef
MIDDLEEAST\t-samota <saeedmotamed@hotmail.com>
2017-06-18 19:03:01 +0400

    PickerData Class Added

--- MSRSale/MSRSalePage.xaml.cs
+++ MSRSale/MSRSalePage.xaml.cs
@@ -6,16 +6,47 @@ namespace MSRSale
 {
     public partial class MSRSalePage : ContentPage
     {
+    
+
         public MSRSalePage()
         {
             InitializeComponent();
             loadPickerData();
-
+            
+            
+            
         }
 
         private void loadPickerData()
         {
-    
+            PickerData pData = new PickerData();
+
+            pData.Location = new List<string>(new[] { "SharafDG", "Jumbo", "Carrefour" });
+
+            pData.Category = new List<string>(new[] { "Catergory1", "Category2" });
+
+
+
+
+            LocationPicker.ItemsSource = pData.Location;
+            CategoryPicker.ItemsSource = pData.Category;
+
+
+
         }
+
+        private void submitBtn_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void LocationPicker_SelectedIndexChanged(object sender, EventArgs e)
+        {
+          
+
+        }
+
+ 
+
     }
 }
---
+++ MSRSale/PickerData.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MSRSale
+{
+    class PickerData
+    {
+        public DateTime Date { get; set; }
+        public List<string> Location { get; set; }
+        public List<string> Account { get; set; }
+        public List<string> Category { get; set; }
+        public List<string> SKU { get; set; }
+
+    }
+}
